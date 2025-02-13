38bf513ff8e7e60508bd40bab68015f85031851b
Trey Bertram <113378171+treybertram06@users.noreply.github.com>
2025-02-12 20:01:43 -0800

    Abstracted out image writing logic

--- include/image.h
+++ include/image.h
@@ -5,7 +5,53 @@
 #ifndef IMAGE_H
 #define IMAGE_H
 
+#include "color.h"
+
 class Image {
+public:
+  
+  Image(int image_width, int image_height) : image_width(image_width), image_height(image_height) {
+    image = new Color*[image_height];
+    for (int i = 0; i < image_height; i++) {
+        image[i] = new Color[image_width];
+    }
+
+    Color c = Color(0, 0, 0);
+    for (int i = 0; i < image_height; i++) {
+        for (int j = 0; j < image_width; j++) {
+            image[i][j] = c;
+        }
+    }
+  }
+
+  void output_image() {
+    std::cout << "P3\n";
+    std::cout << image_width << " " << image_height << std::endl;
+    std::cout << "255\n";
+      
+    Color color;
+    for (int i = 0; i < (image_width*image_height); i++) {
+        for (int i = 0; i < image_height; i++) {
+            for (int j = 0; j < image_width; j++) {
+                std::cout << image[i][j].r << ' ' << image[i][j].g << ' ' << image[i][j].b << std::endl;
+            }
+        }
+    }
+  }
+  
+  
+  void cleanup() {
+    for (int i = 0; i < image_height; i++) {
+        delete[] image[i];
+    }
+    delete[] image;
+  }
+  
+  int image_width;
+  int image_height;
+  
+  Color** image;
+
 
 };
 
--- main.cpp
+++ main.cpp
@@ -1,29 +1,12 @@
 #include <iostream>
 #include "include/color.h"
+#include "include/image.h"
 using namespace std;
 
 
+void put_pixel(int x, int y, Color& c, Image& image) { image.image[x][y] = c; }
 
-void output_image(int iw, int ih, Color** image) {
-    cout << "P3\n";
-    cout << iw << " " << ih << endl;
-    cout << "255\n";
-
-    Color color;
-    for (int i = 0; i < (iw*ih); i++) {
-
-    }
-
-    for (int i = 0; i < ih; i++) {
-        for (int j = 0; j < iw; j++) {
-            cout << image[i][j].r << ' ' << image[i][j].g << ' ' << image[i][j].b << endl;
-        }
-    }
-}
-
-void put_pixel(int x, int y, Color c, Color** image) { image[x][y] = c; }
-
-void draw_line(pair<int, int> P0, pair<int, int> P1, Color c, Color** image) {
+void draw_line(pair<int, int> P0, pair<int, int> P1, Color c, Image image) {
     double a = double(P1.second - P0.second) / double(P1.first - P1.first);
     double b = P0.second - a * P0.first;
 
@@ -34,46 +17,28 @@ void draw_line(pair<int, int> P0, pair<int, int> P1, Color c, Color** image) {
 
 }
 
-Color** initialize_image(int iw, int ih) {
-    Color** image = new Color*[ih];
-    for (int i = 0; i < ih; i++) {
-        image[i] = new Color[iw];
-    }
 
-    Color c = Color(0, 0, 0);
-    for (int i = 0; i < ih; i++) {
-        for (int j = 0; j < iw; j++) {
-            image[i][j] = c;
-        }
-    }
 
-    return image;
-}
 
-void cleanup(Color** image, int ih) {
-    for (int i = 0; i < ih; i++) {
-        delete[] image[i];
-    }
-    delete[] image;
-}
 
 int main() {
 
     int image_width  = 100;
     int image_height = 100;
 
-    Color** image = initialize_image(image_width, image_height);
+    Image image = Image(image_width, image_height);
 
     //draw_line({15, 15}, {75, 75}, {255, 0, 0}, image);
 
-    put_pixel(50, 50, {255, 0, 0}, image);
+    Color red = Color(255, 0, 0);
+    put_pixel(50, 50, red, image);
 
-    clog << "calling output image\n";
-    output_image(image_width, image_height, image);
+    clog << "Writing image to file...\n";
+    image.output_image();
 
 
 
-    cleanup(image, image_height);
+    image.cleanup();
 
     return 0;
 }
